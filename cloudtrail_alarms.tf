9648f4dfdba29936ad0d28db47112cc4a865547c
Dan <dangibbo@amazon.com>
2020-11-15 15:30:32 +0000

    Fix SNS ARN

--- cloudtrail_alarms.tf
+++ cloudtrail_alarms.tf
@@ -3,7 +3,7 @@ data "aws_region" "current" {}
 
 locals {
   alert_for     = "CloudTrailBreach"
-  sns_topic_arn = var.create_sns_topic ? aws_sns_topic.sns_topic_default[0].arn : var.cloudtrail_sns_topic
+  sns_topic_arn = var.create_sns_topic == true ? aws_sns_topic.sns_topic_default[0].arn : var.cloudtrail_sns_topic
   endpoints     = distinct(compact(concat(list(local.sns_topic_arn), var.additional_endpoint_arns)))
   region        = data.aws_region.current.name  #"${var.region == "" ? data.aws_region.current.name : var.region}"
 
