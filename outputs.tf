1a87a4732488ec8dacce1960578642c77be7fcae
Dan <dangibbo@amazon.com>
2020-11-15 15:13:47 +0000

    Fix create SNS topic

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,8 @@ output "support_iam_role_name" {
   description = "The name of the IAM role used for the support user."
   value       = aws_iam_role.support.name
 }
+
+output "sns_arn" {
+  description = "Arn of the SNS topic"
+  value       = concat(aws_sns_topic.sns_topic_default.*.arn, [""])[0]
+}
